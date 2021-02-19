e08068a8135b87c5c07698e7bf7539f7bcbb470c
Sara Nolen <nolensara1@gmail.com>
2021-02-19 18:20:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,8 @@ def create_project_hash
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title: project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
 
 projects
@@ -19,7 +20,7 @@ create_project_hash
 
 
 #projects: kickstarter.css("li.project.grid_4")
-#title: project.css("h2.bbcard_name strong a").text
+
 #image_link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text
