1fe8184e7714b078c0b738723efab76ce0048830
Sara Nolen <nolensara1@gmail.com>
2021-02-19 18:18:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,8 +11,8 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
-  
-  
+
+
 end
 
 create_project_hash
@@ -22,5 +22,5 @@ create_project_hash
 #title: project.css("h2.bbcard_name strong a").text
 #image_link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
-#location: project.css("ul.project-meta span.location-name").text 
+#location: project.css("ul.project-meta span.location-name").text
 #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
